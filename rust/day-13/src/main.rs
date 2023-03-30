a64e3abf16f66ca3e66fc0a2511baaccb9989505
Maitreya Venkataswamy <maitreyakv@gmail.com>
2023-03-30 15:40:43 -0400

    Adds day-13 solution in Rust

--- rust/day-13/src/main.rs
+++ rust/day-13/src/main.rs
@@ -1,9 +1,38 @@
+use std::{ fmt, fs };
 use std::collections::VecDeque;
-use std::fmt;
+use std::cmp::Ordering;
+use std::iter::zip;
 
+#[derive(Clone, Eq, Ord)]
 enum Packet {
     Integer(usize),
     List(Vec<Packet>)
+}
+
+fn split_packet_string(chars: VecDeque<char>) -> Vec<String> {
+    let mut result = Vec::new();
+
+    let mut buf = Vec::new();
+    let mut bracket_count = 0;
+
+    for c in chars {
+        if c == '[' {
+            bracket_count += 1;
+        } else if c == ']' {
+            bracket_count -= 1;
+        }
+
+        if c == ',' && bracket_count == 0 {
+            result.push(String::from_iter(buf.drain(..)));
+
+        } else {
+            buf.push(c);
+        }
+    }
+
+    result.push(String::from_iter(buf.drain(..)));
+
+    result
 }
 
 impl Packet {
@@ -13,18 +42,72 @@ impl Packet {
             chars.pop_front();
             chars.pop_back();
 
-            let mut packet = Vec::new();
-
-            // TODO: Implement
-            while !chars.is_empty() {
-                let c = chars.pop_front();
-                dbg!(c);
-            }
-
+            let packet = if chars.is_empty() {
+                Vec::new()
+            } else {
+                split_packet_string(chars)
+                    .iter()
+                    .map(|p| Packet::from_string(p))
+                    .collect()
+            };
 
             return Packet::List(packet)
         } else {
             return Packet::Integer(s.parse::<usize>().unwrap())
+        }
+    }
+}
+
+impl PartialEq for Packet {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Packet::Integer(x), Packet::Integer(y)) => {
+                return x == y
+            },
+            (Packet::List(x), Packet::List(y)) => {
+                for (l, r) in zip(x.iter(), y.iter()) {
+                    if l != r {
+                        return false
+                    }
+                }
+                return x.len() == y.len()
+            }
+            (Packet::Integer(_), Packet::List(_)) => {
+                let x = Packet::List(vec![self.clone()]);
+                return x == *other
+            },
+            (Packet::List(_), Packet::Integer(_)) => {
+                let y = Packet::List(vec![other.clone()]);
+                return *self == y
+            }
+        }
+    }
+}
+
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match (self, other) {
+            (Packet::Integer(x), Packet::Integer(y)) => {
+                return x.partial_cmp(y)
+            },
+            (Packet::List(x), Packet::List(y)) => {
+                for (l, r) in zip(x.iter(), y.iter()) {
+                    if l < r {
+                        return Some(Ordering::Less)
+                    } else if l > r {
+                        return Some(Ordering::Greater)
+                    }
+                }
+                return x.len().partial_cmp(&y.len())
+            }
+            (Packet::Integer(_), Packet::List(_)) => {
+                let x = Packet::List(vec![self.clone()]);
+                return x.partial_cmp(other)
+            },
+            (Packet::List(_), Packet::Integer(_)) => {
+                let y = Packet::List(vec![other.clone()]);
+                return self.partial_cmp(&y)
+            }
         }
     }
 }
@@ -40,7 +123,9 @@ impl fmt::Display for Packet {
                     string.push_str(&packet.to_string());
                     string.push(',');
                 }
-                string.pop();
+                if string.chars().last().unwrap() == ',' {
+                    string.pop();
+                };
                 string.push(']')
             }
         }
@@ -48,17 +133,52 @@ impl fmt::Display for Packet {
     }
 }
 
+fn read_packet_pairs() -> Vec<(Packet, Packet)> {
+    let file = fs::read_to_string("test_input.txt").unwrap();
+    let mut lines = file.lines().peekable();
+    let mut packet_pairs = Vec::new();
+    while lines.peek().is_some() {
+        let packet_1 = Packet::from_string(lines.next().unwrap());
+        let packet_2 = Packet::from_string(lines.next().unwrap());
+        lines.next();
+        packet_pairs.push((packet_1, packet_2))
+    }
+    packet_pairs
+}
+
+fn read_all_packets() -> Vec<Packet> {
+    fs::read_to_string("test_input.txt")
+        .unwrap()
+        .lines()
+        .filter(|l| l.len() > 1)
+        .map(|l| Packet::from_string(l))
+        .collect()
+}
+
 fn main() {
-    let x = Packet::from_string("[[1],2,3,[4,5],6]");
-    // let x = Packet::List(vec![
-    //     Packet::List(vec![
-    //         Packet::Integer(1)
-    //     ]),
-    //     Packet::List(vec![
-    //         Packet::Integer(2),
-    //         Packet::Integer(3),
-    //         Packet::Integer(4)
-    //     ])
-    // ]);
-    println!("{x}");
+    let packet_pairs = read_packet_pairs();
+    let result: usize = packet_pairs
+                    .iter()
+                    .enumerate()
+                    .map(|(i, pair)| (i + 1, pair))
+                    .filter(|(_, pair)| pair.0 < pair.1)
+                    .map(|(i, _)| i)
+                    .sum();
+    println!("Sum of ordered packet pair indices: {result}");
+
+    let mut packets = read_all_packets();
+
+    let div_1 = Packet::from_string("[[2]]");
+    let div_2 = Packet::from_string("[[6]]");
+
+    packets.push(div_1.clone());
+    packets.push(div_2.clone());
+    packets.sort();
+
+    let result: usize = (0..packets.len())
+        .filter(move |&i| packets[i] == div_1 || packets[i] == div_2)
+        .map(|i| i + 1)
+        .product();
+
+    println!("Product of divider packet indices: {result}");
 }
